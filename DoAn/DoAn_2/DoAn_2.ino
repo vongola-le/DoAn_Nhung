f6cbea17dedb2d83b89af49d2309ad89a3d048ee
Lý Anh Hào <haoly01265820192@gmail.com>
2023-11-15 14:01:49 +0700

    Update

--- DoAn/DoAn_2/DoAn_2.ino
+++ DoAn/DoAn_2/DoAn_2.ino
@@ -3,64 +3,75 @@ Servo myservo; // biến tên myservo của động cơ servo
 
 //cảm biến đèn
 int cambien = 5;
-int led_den = 7;
+int led_den = 2; 
 int gt;
-// cảm biến nhiệt
-int led_nhiet = 8; // Chân kết nối với đèn LED
+int btn_khancap = 1;
+int cambien_last_state = 0;
+
+unsigned long time = 0;
+
+bool btn_khancap_clicked = false;
+// 0 là trạng thái cơ bản, 1 là báo cháy (chớp tắt) (Single click), 2 là mở tất cả đèn (Double click), 3 là tắt hết tất cả đèn (Long-press)
+int btn03_state = 0;
+int led_state = 0;
+bool alert = false;
+
 //Cửa
 int servo = 6; // chân số 6
 int bientro = A0; // chân A0
 int gtbientro; //lưu giá trị biến trở
 int vtservo; // lưu giá trị đọc từ biến trở đổi sang góc
+int lastvtservo = 0;
+int bamxung;
 
 
 void setup() {
   //led
   pinMode(cambien,INPUT);
   pinMode(led_den,OUTPUT);
-  //nhiệt
-  pinMode(led_nhiet, OUTPUT);
+  pinMode(btn_khancap, INPUT);
    //servo
-  myservo.attach(servo);
-  Serial.begin(9600); // Thiết lập chân ledPin là OUTPUT
- 
- 
+  myservo.attach(6);
+
+  cambien_last_state = digitalRead(cambien);
 }
 
 void loop() {
   // biến chung
   gtbientro = analogRead(bientro);
+  bamxung = map(gtbientro,0,1023,0,255);
   gt = digitalRead(cambien);
-  vtservo = map(gtbientro,0,1023,0,180);
-  float temp = (gtbientro *5.0/1024.0)*100.0 ; // Chuyển đổi giá trị cảm biến thành nhiệt độ
   //cảm biến đèn
-  if(gt==1)
+  if(gt==0)
   {
-    Serial.print("gt: ");
-    Serial.println(gt);
-    Serial.print(" led - off");
-    delay(1000);
-    digitalWrite(led_den,0);
+    if(millis() - time >= 500){
+      digitalWrite(led_den, HIGH);
+
+      cambien_last_state = gt;
+
+      time = millis();
+    }
   }
-  else
+  else if(gt == 1)
   {
-    Serial.print("gt: ");
-    Serial.println(gt);
-    Serial.print(" led - on");
-    digitalWrite(led_den,1);
+    if(millis() - time >= 5000){
+      digitalWrite(led_den, LOW);
+
+      cambien_last_state = gt;
+
+      time = millis();
+    }
   }
-  //cảm biến nhiệt
-  Serial.print("temp: ");
-  Serial.println(temp);
-  if (temp > 40) { // Nếu nhiệt độ lớn hơn 40°C
-    digitalWrite(led_nhiet, 1); // Bật đèn LED
-  } else {
-    digitalWrite(led_nhiet, 0); // Tắt đèn LED
+
+  if(bamxung <= 50){
+    myservo.write(85);
   }
-  delay(1000); // Đợi 1 giây
-  //servo
-  myservo.write(vtservo);
-  Serial.print("góc: ");
-  Serial.println(vtservo);
-  delay(500);
+
+  if(bamxung > 50 && bamxung < 205){
+    myservo.write(90);
+  }
+
+  if(bamxung >= 205){
+    myservo.write(100);
+  }
 }
