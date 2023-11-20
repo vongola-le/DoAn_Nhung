f71f84ccb3bad5c5773768f3e4be67233d8c3325
LeNgocBaThong <ngunguoi0909@gmail.com>
2023-11-20 14:00:56 +0700

    Update  TB_Screen
    chỉnh sửa cho gọn lại

--- DoAn/app_smart_house/lib/TB_Screen.dart
+++ DoAn/app_smart_house/lib/TB_Screen.dart
@@ -1,9 +1,44 @@
 import 'package:flutter/material.dart';
 
+class Info_ThongBao extends StatelessWidget {
+  const Info_ThongBao({super.key,required this.tieude,required this.noidung});
+  final String? tieude;
+  final String? noidung;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8.0),
+      child: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+        color: Colors.grey,
+        border: Border.all(
+        color: Colors.black,
+        width: 2.0,
+        ),
+        borderRadius: BorderRadius.circular(10.0)
+        ),
+        child: Column(
+          children: [
+            Text(
+              '$tieude',
+              style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 5.0),
+            Text(
+              '$noidung',
+              style: TextStyle(fontSize: 18.0),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class TB_Screen extends StatelessWidget {
-  const TB_Screen({super.key,required this.text,required this.value});
-  final String text;
-  final String value;
+  const TB_Screen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,275 +51,32 @@ class TB_Screen extends StatelessWidget {
         ],
       ),
       
-      body: SingleChildScrollView(
-        child: Padding(
-          padding:const EdgeInsets.all(10),
-          child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 10),
-            Container(
-                  width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 2.0,
-                    ),
-                    borderRadius: BorderRadius.circular(8.0)
-              ),
-              child: const Column (
-                children: [
-                  Text(
-                    'Thông báo 1',
-                    style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold),
+      body: ListView(
+        children: [
+          Column(
+            children: [
+              Padding(
+                padding:const EdgeInsetsDirectional.all(10),
+                child: Container(
+                  child: const Column(
+                    children: [
+                      Info_ThongBao(tieude: 'Thông báo 1',noidung: 'Nội dung thông báo 1',),
+                      Info_ThongBao(tieude: 'Thông báo 2',noidung: 'Nội dung thông báo 2',),
+                      Info_ThongBao(tieude: 'Thông báo 3',noidung: 'Nội dung thông báo 3',),
+                      Info_ThongBao(tieude: 'Thông báo 4',noidung: 'Nội dung thông báo 4',),
+                      Info_ThongBao(tieude: 'Thông báo 5',noidung: 'Nội dung thông báo 5',),
+                      Info_ThongBao(tieude: 'Thông báo 6',noidung: 'Nội dung thông báo 6',),
+                      Info_ThongBao(tieude: 'Thông báo 7',noidung: 'Nội dung thông báo 7',),
+                      Info_ThongBao(tieude: 'Thông báo 8',noidung: 'Nội dung thông báo 8',),
+                      Info_ThongBao(tieude: 'Thông báo 9',noidung: 'Nội dung thông báo 9',),
+                      Info_ThongBao(tieude: 'Thông báo 10',noidung: 'Nội dung thông báo 10',),
+                    ],
                   ),
-                  SizedBox(height: 5.0),
-                  Text(
-                    'Nội dung thông báo 1',
-                    style: TextStyle(fontSize: 18.0),
-                  ),
-                ],
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 2.0,
-                    ),
-                    borderRadius: BorderRadius.circular(8.0)
-              ),
-              child: const Column(
-                children: [
-                  Text(
-                    'Thông báo 2',
-                    style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 5.0),
-                  Text(
-                    'Nội dung thông báo 2',
-                    style: TextStyle(fontSize: 18.0),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 2.0,
-                    ),
-                    borderRadius: BorderRadius.circular(8.0)
-              ),
-              child: const Column(
-                children: [
-                  Text(
-                    'Thông báo 3',
-                    style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 5.0),
-                  Text(
-                    'Nội dung thông báo 3',
-                    style: TextStyle(fontSize: 18.0),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 2.0,
-                    ),
-                    borderRadius: BorderRadius.circular(8.0)
-              ),
-              child: const Column(
-                children: [
-                  Text(
-                    'Thông báo 4',
-                    style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 5.0),
-                  Text(
-                    'Nội dung thông báo 4',
-                    style: TextStyle(fontSize: 18.0),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 2.0,
-                    ),
-                    borderRadius: BorderRadius.circular(8.0)
-              ),
-              child: const Column(
-                children: [
-                  Text(
-                    'Thông báo 5',
-                    style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 5.0),
-                  Text(
-                    'Nội dung thông báo 5',
-                    style: TextStyle(fontSize: 18.0),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 2.0,
-                    ),
-                    borderRadius: BorderRadius.circular(8.0)
-              ),
-              child: const Column(
-                children: [
-                  Text(
-                    'Thông báo 6',
-                    style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 5.0),
-                  Text(
-                    'Nội dung thông báo 6',
-                    style: TextStyle(fontSize: 18.0),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 2.0,
-                    ),
-                    borderRadius: BorderRadius.circular(8.0)
-              ),
-              child: const Column(
-                children: [
-                  Text(
-                    'Thông báo 7',
-                    style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 5.0),
-                  Text(
-                    'Nội dung thông báo 7',
-                    style: TextStyle(fontSize: 18.0),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 2.0,
-                    ),
-                    borderRadius: BorderRadius.circular(8.0)
-              ),
-              child: const Column(
-                children: [
-                  Text(
-                    'Thông báo 8',
-                    style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 5.0),
-                  Text(
-                    'Nội dung thông báo 8',
-                    style: TextStyle(fontSize: 18.0),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 2.0,
-                    ),
-                    borderRadius: BorderRadius.circular(8.0)
-              ),
-              child: const Column(
-                children: [
-                  Text(
-                    'Thông báo 9',
-                    style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 5.0),
-                  Text(
-                    'Nội dung thông báo 9',
-                    style: TextStyle(fontSize: 18.0),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20),
-            Container(
-              width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.all(10),
-                  decoration: BoxDecoration(
-                    color: Colors.grey,
-                    border: Border.all(
-                      color: Colors.black,
-                      width: 2.0,
-                    ),
-                    borderRadius: BorderRadius.circular(8.0)
-              ),
-              child: const Column(
-                children: [
-                  Text(
-                    'Thông báo 10',
-                    style: TextStyle(fontSize: 24.0,fontWeight: FontWeight.bold),
-                  ),
-                  SizedBox(height: 5.0),
-                  Text(
-                    'Nội dung thông báo 10',
-                    style: TextStyle(fontSize: 18.0),
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 20)
-          ],
-        ),)
+            ],
+          )
+        ],
       ),
     );
   }
