f143caa4fc26bb7df373eb213e6a49a77980133e
Đặng Hoàn Phương <146814457+DangHoanPhuong@users.noreply.github.com>
2023-12-04 17:03:52 +0700

    update button realtime

---
+++ DoAn/app_smart_house/lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:app_smart_house/items/auth_page.dart';
+import 'package:app_smart_house/items/sign_in.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      // home: LoginScreen(),
+      home: const AuthPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
